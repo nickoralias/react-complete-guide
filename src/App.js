dcee5b12f83c3a1dce2b9f4b225d7ae78a18c659
Nick Lofaro <5376991+nickoralias@users.noreply.github.com>
2020-06-26 18:42:53 -0700

    Add Person component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 function App() {
   return (
     <div className="App">
       <h1>Hi, this is a React App</h1>
+      <Person name="Nick" age="29"/>
+      <Person name="Manu" age="29">My Hobbies: Racing</Person>
+      <Person name="Stephanie" age="26"/>
     </div>
   );
 }
