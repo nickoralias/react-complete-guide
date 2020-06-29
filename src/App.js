17ee4de14f672eb26449313e4b62eb21a6e1ee7b
Nick Lofaro <5376991+nickoralias@users.noreply.github.com>
2020-06-28 23:59:30 -0700

    Render content conditionally

--- src/App.js
+++ src/App.js
@@ -8,12 +8,11 @@ const App = props => {
       { name: "Nick", age: 29 },
       { name: "Manu", age: 29 },
       { name: "Stephanie", age: 26 },
-    ]
+    ],
+    showPeople: false
   });
 
   const [otherState, setOtherState] = useState('some other value');
-
-  console.log(peopleState, otherState);
 
   const switchNameHandler = (newName) => {
     // console.log('Was clicked!');
@@ -37,21 +36,44 @@ const App = props => {
     });
   }
 
+  const togglePeopleHandler = () => {
+    const doesShow = peopleState.showPeople;
+    setPeopleState({
+      people: peopleState.people,
+      showPeople: !doesShow
+    });
+  }
+
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  };
+
   return (
     <div className="App">
       <h1>Hi, this is a React App</h1>
-      <button onClick={() => switchNameHandler('Nicholas!!')}>Switch Name</button>
-      <Person
-        name={peopleState.people[0].name}
-        age={peopleState.people[0].age} />
-      <Person
-        name={peopleState.people[1].name}
-        age={peopleState.people[1].age}
-        changed={nameChangedHandler}
-        click={switchNameHandler.bind(this, 'Nick!')}>My Hobbies: Racing</Person>
-      <Person
-        name={peopleState.people[2].name}
-        age={peopleState.people[2].age} />
+      <button
+        style={style}
+        onClick={togglePeopleHandler}>Toggle People</button>
+      {
+        peopleState.showPeople ?
+          <div>
+            <Person
+              name={peopleState.people[0].name}
+              age={peopleState.people[0].age} />
+            <Person
+              name={peopleState.people[1].name}
+              age={peopleState.people[1].age}
+              changed={nameChangedHandler}
+              click={switchNameHandler.bind(this, 'Nick!')}>My Hobbies: Racing</Person>
+            <Person
+              name={peopleState.people[2].name}
+              age={peopleState.people[2].age} />
+          </div> : null
+      }
     </div>
   );
 };
