d6327914925122896f9ea92e146d90dd5498faa4
Nick Lofaro <5376991+nickoralias@users.noreply.github.com>
2020-07-16 20:22:32 -0700

    Use media query

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
-import Radium from 'radium';
+import Radium, { StyleRoot } from 'radium';
 import Person from "./Person/Person";
 
 const App = props => {
@@ -97,14 +97,16 @@ const App = props => {
   }
 
   return (
-    <div className="App">
-      <h1>Hi, this is a React App</h1>
-      <p className={classes.join(' ')}>This is really working!</p>
-      <button
-        style={style}
-        onClick={togglePeopleHandler}>Toggle People</button>
-      {people}
-    </div>
+    <StyleRoot>
+      <div className="App">
+        <h1>Hi, this is a React App</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
+        <button
+          style={style}
+          onClick={togglePeopleHandler}>Toggle People</button>
+        {people}
+      </div>
+    </StyleRoot>
   );
 };
 
