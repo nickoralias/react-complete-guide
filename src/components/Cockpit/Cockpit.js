f0fe5f20f543fbc5b6aaec347af9db9ae7f527af
Nick Lofaro <5376991+nickoralias@users.noreply.github.com>
2020-08-05 01:08:08 -0700

    Cleanup with useEffect

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -6,9 +6,13 @@ const Cockpit = (props) => {
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
     // Http request...
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       alert('Saved data to cloud');
     }, 1000);
+    return () => {
+      clearTimeout(timer);
+      console.log('[Cockpit.js] cleanup work in useEffect');
+    };
   }, []);
 
   // useEffect();
