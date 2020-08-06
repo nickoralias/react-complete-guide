d9c8cf33e79dff67b0226cdad0adb6bdd59e890c
Nick Lofaro <5376991+nickoralias@users.noreply.github.com>
2020-08-05 23:23:33 -0700

    Pass unknown props

--- src/components/People/Person/Person.js
+++ src/components/People/Person/Person.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from "react";
 
 import Aux from "../../../hoc/Aux";
+import withClass from '../../../hoc/withClass';
+import classes from './Person.css';
 
 class Person extends Component {
   render() {
@@ -22,4 +24,4 @@ class Person extends Component {
   }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
