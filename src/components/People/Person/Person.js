092a1060286e9c8110c93a8efb448f86d9a7afe2
Nick Lofaro <5376991+nickoralias@users.noreply.github.com>
2020-08-05 21:45:49 -0700

    Render adjacent JSX components

--- src/components/People/Person/Person.js
+++ src/components/People/Person/Person.js
@@ -1,18 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import classes from './Person.css';
+import Aux from "../../../hoc/Aux";
 
 class Person extends Component {
-    render() {
-        console.log('[Person.js] rendering...');
-        return (
-            <div className={classes.Person}>
-                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}/>
-            </div>
-        );
-    }
+  render() {
+    console.log("[Person.js] rendering...");
+    return (
+      <Aux>
+        <p key="i0" onClick={this.props.click}>
+          I'm {this.props.name} and I am {this.props.age} years old!
+        </p>
+        <p key="i1">{this.props.children}</p>
+        <input
+          key="i2"
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </Aux>
+    );
+  }
 }
 
 export default Person;
