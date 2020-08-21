80b469fad458221b33f631a800e3e6ed4ab7e15b
Nick Lofaro <5376991+nickoralias@users.noreply.github.com>
2020-08-20 23:54:42 -0700

    Use contextType and useContext

--- src/components/People/Person/Person.js
+++ src/components/People/Person/Person.js
@@ -12,20 +12,23 @@ class Person extends Component {
     this.inputElementRef = React.createRef();
   }
 
+  static contextType = AuthContext;
+
   componentDidMount() {
     // this.inputElement.focus();
     this.inputElementRef.current.focus();
+    console.log(this.context.authenticated);
   }
 
   render() {
     console.log("[Person.js] rendering...");
     return (
       <Fragment>
-        <AuthContext.Consumer>
-          {(context) =>
-            context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>
-          }
-        </AuthContext.Consumer>
+        {this.context.authenticated ? (
+          <p>Authenticated!</p>
+        ) : (
+          <p>Please log in</p>
+        )}
         <p key="i0" onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
